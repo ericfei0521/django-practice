537d5e8d04b7bef9d87e99c9549477a4c305ec78
Eric Fei <eric.fei@partipost.com>
2023-06-15 22:39:15 +0800

    implement python modal

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from first_app.models import AccessRecords, Topic, WebPage
 
 # Register your models here.
+admin.site.register(AccessRecords)
+admin.site.register(Topic)
+admin.site.register(WebPage)
